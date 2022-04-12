d52d940599aa86f1efb09d70cbc3f69d0944ba29
LanceStasinski <lance.stasinski@gmail.com>
2022-04-12 09:58:19 -0400

    chore: Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+module.exports = {
+  content: ['./app/**/*.hbs'],
+  theme: {
+    extend: {
+      colors: {
+        perfect: '#4dac26',
+        good: '#b8e186',
+        okay: '#f1b6da',
+        poor: '#d01c8b',
+        'wet-suit': '#00E0FF',
+        'gray-1': '#444444',
+        'gray-2': '#666666',
+      },
+      fontFamily: {
+        'Poiret-One': ['Poiret One', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
