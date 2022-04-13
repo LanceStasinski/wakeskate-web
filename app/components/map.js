0d27103584787ad4b7874fb37c3d037e9b40150b
LanceStasinski <lance.stasinski@gmail.com>
2022-04-13 12:03:40 -0400

    feat: Add Modal component

--- app/components/map.js
+++ app/components/map.js
@@ -1,3 +1,35 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import fetch from 'fetch';
+import ENV from 'wakeskate-web-2/config/environment';
 
-export default class MapComponent extends Component {}
+export default class MapComponent extends Component {
+  @tracked query = '';
+  @tracked lat = 0;
+  @tracked lng = 0;
+  @tracked hasSearched = false;
+
+  @action async postSearch(event) {
+    event.preventDefault();
+    console.log(this.query);
+    const data = { location: this.query };
+    const response = await fetch(`${ENV.REST_API}/geocode/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const responseData = await response.json();
+
+    console.log(responseData);
+
+    this.lat = responseData.lat;
+    this.lng = responseData.lng;
+
+    if (this.lat && this.lng) {
+      this.hasSearched = true;
+    }
+  }
+}
