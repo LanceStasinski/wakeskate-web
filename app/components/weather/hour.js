a6574564acac333e32031488e88e3acf08189997
LanceStasinski <lance.stasinski@gmail.com>
2022-04-29 09:59:46 -0400

    feat: Add hourly weather info

--- app/components/weather/hour.js
+++ app/components/weather/hour.js
@@ -1,7 +1,16 @@
 import Component from '@glimmer/component';
 import { iconAlt } from './current';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { htmlSafe } from '@ember/template';
 
 export default class WeatherHourComponent extends Component {
+  @tracked infoIsExpanded = false;
+
+  @action toggleShowInfo() {
+    this.infoIsExpanded = !this.infoIsExpanded;
+  }
+
   get icon() {
     const hour = this.args.hour;
     const image = hour.rating === 'bad' ? 'poor' : hour.rating;
@@ -14,5 +23,28 @@ export default class WeatherHourComponent extends Component {
       date.hour > 12 ? date.hour - 12 : date.hour === 0 ? 12 : date.hour;
     const amPm = date.hour >= 12 ? 'pm' : 'am';
     return { hour, amPm, datetime: date.datetime };
+  }
+
+  get weatherIcon() {
+    const icon = this.args.hour.weatherInfo.icon;
+    const alt = this.args.hour.weatherInfo.description;
+    return {
+      weatherIcon: `http://openweathermap.org/img/wn/${icon}@2x.png`,
+      alt,
+    };
+  }
+
+  get temp() {
+    return Math.floor(this.args.hour.temperature.actual);
+  }
+
+  get wind() {
+    const speed = Math.floor(this.args.hour.wind.wind_speed);
+    const windDeg = this.args.hour.wind.wind_deg;
+    const windDegStyle = htmlSafe(`transform: rotate(${windDeg}deg)`);
+    const gust = Math.floor(this.args.hour.wind.wind_gust);
+    const icon = '/assets/icons/wind.svg';
+    const alt = `Orange arrow pointing ${windDeg} degrees.`
+    return { speed, windDegStyle, icon, alt, gust };
   }
 }
